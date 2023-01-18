140550193619355f91e6c2f62785b438d948ecc3
Igor_PO <89515076779@ya.ru>
2023-01-18 16:28:07 +0300

    change layout for pagination & logic

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -3,21 +3,36 @@ import { useContext } from 'react'
 import { UsersContext } from '../hoc/UsersProvider'
 import { v4 as uuid } from 'uuid'
 export const Pagination = () => {
-    const {currentPageNumber, setCurrentPageNumber, activeColumn,  setTotalNumberPages, limitNumberUsers, totalNumberUsers, totalNumberPages, setLimitNumberUsers,} = useContext(UsersContext)
-    const arrFromTotalNumberPage = Array.from(Array(totalNumberPages), (_, i) => i+1)
+    const {currentPageNumber, setCurrentPageNumber, totalNumberPages} = useContext(UsersContext)
 
-    const handleSetCurruntPage = (e) => {
-        setCurrentPageNumber(e.target.innerText)
+    const handleChangeCurrentPage = (pageValue) => {
+      if (pageValue == '-' & currentPageNumber > 1) {
+        setCurrentPageNumber(currentPageNumber-1)
+      } else if (pageValue == '+' & currentPageNumber < totalNumberPages) {
+        setCurrentPageNumber(currentPageNumber+1)
+      } else if (pageValue === 1) {
+        setCurrentPageNumber(1)
+      } else if (pageValue === -1) {
+        setCurrentPageNumber(totalNumberPages)
+      }
     }
   return (
     <React.Fragment>
-        <nav aria-label="...">
+        <nav className='nav' aria-label="...">
+            <p>{`page ${currentPageNumber} из ${totalNumberPages}`}</p>
             <ul className="pagination-nav">
-               {arrFromTotalNumberPage.map(item=> {
-                return <li  key={uuid()} className="page-item">
-                            <a onClick={(e) => handleSetCurruntPage(e)} className="page-link" href="#">{item}</a>
-                        </li>
-               })}
+              <li  key={uuid()} onClick={() => handleChangeCurrentPage (1) } className="page-item">
+                  <a className="page-link" href="#">{`<<`}</a>
+              </li>
+              <li  key={uuid()} onClick={() => handleChangeCurrentPage ('-') } className="page-item">
+                  <a className="page-link" href="#">{`<`}</a>
+              </li>
+              <li  key={uuid()} onClick={() => handleChangeCurrentPage ('+') } className="page-item">
+                  <a  className="page-link" href="#">{`>`}</a>
+              </li>
+              <li  key={uuid()} onClick={() => handleChangeCurrentPage (-1) } className="page-item">
+                  <a  className="page-link" href="#">{`>>`}</a>
+              </li>
             </ul>
         </nav>
     </React.Fragment>
